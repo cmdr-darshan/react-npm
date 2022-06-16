d9fa3ee337318e6fe27b20b59b257b354b00d6a6
cmdr-darshan <darshan@initcoders.com>
2022-06-16 14:48:05 +0530

    final commit for react

---
+++ dist/components/dummy/index.js
@@ -0,0 +1,17 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function Dummy() {
+  return /*#__PURE__*/_react.default.createElement("h1", null, "Hi I am Darshan");
+}
+
+var _default = Dummy;
+exports.default = _default;
